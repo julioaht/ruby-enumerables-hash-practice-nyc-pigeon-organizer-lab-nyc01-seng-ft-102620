4a85ad076ab60f3be888f0056be765d779891d5e
julio hernandez <julioandres72@gmail.com>
2020-10-21 14:23:23 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,6 +1,20 @@
 require "pry"
 
 def nyc_pigeon_organizer(data)
+  data.each_with_object({}) do |(key, value), final_array|
+    value.each do |inner_key, names|
+      names.each do |name|
+        if !final_array[name]
+          final_array[name] = {}
+        end
+        if !final_array[name][key]
+          !final_array[name][key] = []
+        end
+
+      end
+    end
+    final_array
+  end
 
 binding.pry  
 end
